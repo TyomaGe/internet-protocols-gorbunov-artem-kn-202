aa250f13574fa040d787d24ca4663cd5589da5d6
Tyoma Gorbunov <bivaetctoskazat123@gmail.com>
2025-03-31 18:48:42 +0500

    Трассировка автономных систем

--- Трассировка автономных систем/find_host_info.py
+++ Трассировка автономных систем/find_host_info.py
@@ -1,12 +1,9 @@
 import re
-
 import requests
-from name_matching import get_ip
 
 
 def info_seeker(host: str) -> dict:
     try:
-        host = get_ip(host)
         response = requests.get(f"http://ip-api.com/json/{host}").json()
         result = {
             "query": response.get("query", "Нет данных"),
--- Трассировка автономных систем/name_matching.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-def get_ip(host: str) -> str | None:
-    try:
-        ip = socket.gethostbyname(host)
-        return ip
-    except (socket.gaierror, socket.herror):
-        return None
